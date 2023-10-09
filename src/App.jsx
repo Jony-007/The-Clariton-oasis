7c49e1e70027deab054ab9980b96f5883af9f755
Jony-007 <habiburrahmanjony4@gmail.com>
2023-10-08 22:25:11 -0400

    Added Button and Row reusable styled components

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import GlobalStyles from "./styles/GlobalStyles";
 import Button from "./ui/Button";
 import Input from "./ui/Input";
 import Heading from "./ui/Heading";
+import Row from "./ui/Row";
 
 const StyledApp = styled.div`
-  background-color: red;
   padding: 2px;
 `;
 
@@ -15,14 +15,37 @@ export const App = () => {
     <>
       <GlobalStyles />
       <StyledApp>
-        <Heading type="h1"> The wild oasis</Heading>
-        <Heading type="h2"> Check in and out</Heading>
+        <Row type="vertical">
+          <Row type="horizontal">
+            <Heading type="h1"> The wild oasis</Heading>
+            <div>
+              <Heading type="h2"> Check in and out</Heading>
 
-        <Button onClick={() => alert("Pikachu")}>Check in</Button>
-        <Button onClick={() => alert("Pikachu")}>Check in</Button>
-        <Heading type="h3"> Form</Heading>
+              <Button
+                variation="primary"
+                size="medium"
+                onClick={() => alert("Check in")}
+              >
+                Check in
+              </Button>
+              <Button
+                variation="secondary"
+                size="small"
+                onClick={() => alert("Check Out")}
+              >
+                Check out
+              </Button>
+            </div>
+          </Row>
 
-        <Input type="number" placeholder="Number of guests" />
+          <Row type="vertical">
+            <Heading type="h3"> Form</Heading>
+            <form>
+              <Input type="number" placeholder="Number of guests" />
+              <Input type="number" placeholder="Number of guests" />
+            </form>
+          </Row>
+        </Row>
       </StyledApp>
     </>
   );
